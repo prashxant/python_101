9924f3584c4826d0cf729b4ccfa53e1e9f9b7c5c
notxdead0@gmail.com <notxdead0@gmail.com>
2024-08-21 08:19:46 +0530

    3
    3

---
+++ 02_strings/skipval.py
@@ -0,0 +1,3 @@
+a = "0123456789"
+b=a[1:8:2]  # 1 to 8 with step 2 is the process for skip value indices
+print(b)
--- 02_strings/str_slice.py
+++ 02_strings/str_slice.py
@@ -1,5 +1,9 @@
 name = "Prashant"
 
 nameshort = name[0:4]
+#nameshort = name[:4]   (check)
+#nameshort = name[1:]   (check)
  
 print(nameshort)
+
+# also check for negative index
