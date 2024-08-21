a1bd9802ccd4568a95fa84e65236f46e85d456aa
notxdead0@gmail.com <notxdead0@gmail.com>
2024-08-21 08:45:21 +0530

    5
    5

---
+++ 02_strings/escap_seq.py
@@ -0,0 +1,4 @@
+
+a = "Prashant is aa good boy \n but not a bad \"boy\" OKAY"
+
+print(a)
--- 02_strings/stringfunctins.py
+++ 02_strings/stringfunctins.py
@@ -4,3 +4,9 @@ name =  "Prashant"
 print(len(name))
 print(name.endswith("antt")) #False
 print(name.endswith("ant")) #True
+
+print (name.startswith("Pras")) #True
+print(name.capitalize())
+
+
+#a.replace use karo
