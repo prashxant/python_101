a75a97a52ff120bedf80dc60e115c2c5dbd0fc08
notxdead0@gmail.com <notxdead0@gmail.com>
2024-08-22 19:28:29 +0530

    11
    dd

---
+++ 03_lists&tupes/list.py
@@ -0,0 +1,7 @@
+# unlike strings list are mutable
+
+
+friends = ["apple", "banana", "cherry",4,2,"nanapatekar"]
+
+
+print(friends[0:4])
---
+++ 03_lists&tupes/listmethods.py
@@ -0,0 +1,16 @@
+friends = ["apple", "banana", "cherry",4,2,"nanapatekar"]
+
+friends.append("damodar")
+
+friends.reverse("damodar")
+friends.insert("damodar",4)
+friends.pop("damodar")
+friends.remove("damodar")
+
+print(friends)
+
+
+l1 = [1, 8, 7, 2, 21, 15]
+l1.sort()
+l1.reverse()
+print(l1) # sorts the list
