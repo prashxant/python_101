9ffd3e858acc612657779afe1d9678980b3a32f7
notxdead0@gmail.com <notxdead0@gmail.com>
2024-09-25 07:12:47 +0530

    while loops
    1

---
+++ loops/list_using_whileloop.py
@@ -0,0 +1,8 @@
+l = [1 , "Harry ", "potter", 4, 5, 6, 7, 8, 9, 10 , "lorem", "ipsum", "dolor", "sit", "amet"]
+
+i = 0
+
+while (i< len(l)):
+    print(l[i])
+
+    i +=1
--- loops/loops.py
+++ loops/loops.py
@@ -1,4 +1,4 @@
 
 
-for i in  range(1 , 6):
-    print(i)
+for i in  range(1 , 10):
+    print(i )
