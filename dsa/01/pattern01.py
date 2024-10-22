b11c45563d9aa83bb9d8e5f5024ebca10614581b
notxdead0@gmail.com <notxdead0@gmail.com>
2024-10-22 20:48:17 +0530

    i
    k

---
+++ dsa/01/pattern01.py
@@ -0,0 +1,86 @@
+n = int(input("Enter the number of rows/columns: "))
+
+
+
+
+
+
+# * * * * *  
+# * * * * *
+# * * * * *
+# * * * * *
+
+# for i in range(n):
+#     for j in range(n):
+#         print("*", end= " ")
+#     print(" ")
+
+
+
+
+
+# *
+# **
+# ***
+# ****
+# *****
+
+
+# for i in range (1, n+1):
+#     for j in range(i):
+#         print("*", end="")
+#     print("")
+
+
+
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(j+1,end="")
+#     print("")
+
+
+
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(i,end="")
+#     print("")
+
+# *****
+# ****
+# ***
+# **
+# *
+
+
+# for i in range(n):
+#     for j in range(n-i):
+#         print("*",end="")
+#     print("")
+
+
+
+    # 12345
+    # 1234
+    # 123
+    # 12
+    # 1
+
+    # for i in range (n):
+    #     for j in range (n-i):
+    #         print(j+1 , end="")
+    #     print("")
--- new/hi.py
+++
@@ -1,5 +0,0 @@
-
-a = ()
-
-
-print(type(a))
